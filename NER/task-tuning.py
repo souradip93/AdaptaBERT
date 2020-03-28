6cbc364d0654665d5ae451d5b06bb3eff3adeb30
Souradip Guha <sourya2793@gmail.com>
2020-03-28 10:02:42 +0530

    Update

--- NER/task-tuning.py
+++ NER/task-tuning.py
@@ -609,7 +609,7 @@ def main():
         for _ in trange(int(args.num_train_epochs), desc="Epoch"):
             tr_loss = 0
             nb_tr_examples, nb_tr_steps = 0, 0
-            for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
+            for step, batch in enumerate(tqdm(train_dataloader)):
                 batch = tuple(t.to(device) for t in batch)
                 input_ids, input_mask, segment_ids, label_ids, label_mask = batch
                 loss = model(input_ids, segment_ids, input_mask, label_ids, label_mask)
